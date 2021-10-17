056eceb732a3483908a287b1a641a1d5ed395f55
kanta.kaneki <hotcola2488@gmail.com>
2021-10-17 12:44:48 +0000

    fixed4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
